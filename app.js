0ce2cb285602da9dde12279e9a49bcb41a8ca44a
Eduhj <[eduardo.jonerhahn@gmail.com](mailto:eduardo.jonerhahn@gmail.com)>
2026-05-21 19:10:32 -0300

    Correção de adição de features para a branch 'funcional'

--- app.js
+++ app.js
@@ -1,18 +1,12 @@
 document.addEventListener("click", function (e) {
 
-    const res = document.getElementById("res")
-
-    const a = document.getElementById("a").value.trim()
-    const b = document.getElementById("b").value.trim()
-
-    const an = Number(a)
-    const bn = Number(b)
+    
 
     const el = e.target
+    console.log(el.value)
 
-    if (el.id === "soma") res.textContent = Soma(an, bn)
-    if (el.id === "subt") res.textContent = Subt(an, bn)
-    if (el.id === "mult") res.textContent = Mult(an, bn)
-    if (el.id === "frac") res.textContent = Frac(an, bn)
-
+    if (el.value == undefined || el.name == "ignore") {
+        if (el.value == "CE") display.value = ""
+    }
+    else display.value += String(el.value)
 })
